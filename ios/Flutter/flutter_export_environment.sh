9d44866dfc3663e73e6b575b3bec420c264925c0
Moises Goncalves <moises100hp@gmail.com>
2019-12-28 21:52:08 -0300

    atualizado o framework
    atualizado o framework do flutter

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\moises\source\repos\moises100hp\Flutter\Projetos\calculadora_de_imc"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/moisesgs/Documents/flutter_framework"
+export "FLUTTER_APPLICATION_PATH=/Users/moisesgs/Documents/Projetos_Flutter/Projetos/calculadora_de_imc"
+export "FLUTTER_TARGET=/Users/moisesgs/Documents/Projetos_Flutter/Projetos/calculadora_de_imc/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/moisesgs/Documents/flutter_framework/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
